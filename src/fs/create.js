fba6f9b6b51dd0396556436f4e0818365edf7661
maks-rafalko <b0rn@list.ru>
2022-11-30 16:05:56 +0100

    feat: implement `create.js` logic to create `fresh.txt` file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import { access, constants, writeFile } from 'node:fs/promises';
+import * as url from 'node:url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const fileExists = async (filePath) => {
+    try {
+        await access(filePath, constants.F_OK);
+
+        return true;
+    } catch {
+        return false;
+    }
+}
+
 const create = async () => {
-    // Write your code here 
+    const filePath = __dirname + 'files/fresh.txt';
+
+    const isFileExists = await fileExists(filePath);
+
+    if (isFileExists) {
+        throw new Error('FS operation failed');
+    }
+
+    await writeFile(filePath, 'I am fresh and young')
 };
 
 await create();
